c50e292be7edb6dc3fb1f1c18d9501fa2e4e599d
Prakash <prakashmvp8080@gmail.com>
2024-12-24 05:14:40 +0530

    sav

---
+++ DS_and_A/neetcodeBAsics/Recursion.java
@@ -0,0 +1,16 @@
+package neetcodeBAsics;
+
+public class Recursion {
+
+    
+    private static int fibbo(int n){
+        if( n ==0) return 0;
+        if(n==1)return 1;
+        int l = fibbo(n-1);
+        int r = fibbo(n-2);
+        return l+r;
+    }
+    public static void main(String[] args) {
+        System.out.println(fibbo(5));
+    }
+}
