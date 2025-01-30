329179ec880b66d4f0ee18c171b4005ab1bfa2a9
Prakash <prakashmvp8080@gmail.com>
2025-01-31 03:46:21 +0530

    saving

---
+++ DS_and_A/neetcodeBAsics/bessttimetobuyandsellstocks.java
@@ -0,0 +1,6 @@
+package neetcodeBAsics;
+
+public class bessttimetobuyandsellstocks {
+
+    
+}
---
+++ Skillrack/teamProblem.java
@@ -0,0 +1,18 @@
+package Skillrack;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class teamProblem {
+
+    private static void sol1(){
+        Scanner sc = new Scanner(System.in);
+     
+        ArrayList<Integer> teamA = new ArrayList<>();
+        ArrayList<Integer> teamB = new ArrayList<>();
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
