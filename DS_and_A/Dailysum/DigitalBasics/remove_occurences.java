6455962b72e33d5df0664b0993eca2050afa29b2
Prakash <prakashmvp8080@gmail.com>
2025-04-01 19:14:32 +0530

    saving for bro

---
+++ DS_and_A/Dailysum/DigitalBasics/remove_occurences.java
@@ -0,0 +1,13 @@
+package Dailysum.DigitalBasics;
+
+public class remove_occurences {
+    private static void sol1(int[]arr){
+    }
+
+    public static void main(String[] args) {
+        int[]arr={1, 4, 2, 6, 2, 6, 9, 4};
+        int j=0;
+        System.out.println(arr[j]);
+        System.out.println(arr[j++]);
+    }
+}
